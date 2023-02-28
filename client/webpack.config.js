c971d51c9d3aa7b5d14afd210b42952dce04c0b1
Megan Ellman <meganlellman@gmail.com>
2023-02-28 11:13:07 -0800

    created webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -11,17 +11,57 @@ module.exports = () => {
 
     // TODO: Add the correct output
     output: {
-      
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist'),
     },
 
     // TODO: Add the correct plugins
     plugins: [
-     
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'TODOs List'
+      }),
+
+      new GenerateSW(),
+      new WebpackPwaManifest({
+        
+          filename: "manifest.json",
+          name: "App",
+          orientation: "portrait",
+          display: "standalone",
+          start_url: "./",
+          crossorigin: null,
+          inject: true,
+          fingerprints: true,
+          ios: false,
+          publicPath: "./",
+          includeDirectory: true,
+          icons: {
+              src: path.resolve('./src/images/logo.png'),
+              sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+          }
+      }),
     ],
 
     // TODO: Add the correct modules
     module: {
-
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
+      ],
     }
   };
 };
